1f09dfb8902b1c1369f154246b170865a297ec0e
Justreadin <dave.400g@gmail.com>
2026-01-01 12:45:46 +0100

    almost done

--- frontend/js/dashboard-enhancements.js
+++ frontend/js/dashboard-enhancements.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:54a64e7bfc6341c8faf2830d6d314f1a8d5384e30e724cc1d612eb2d5b6a07c3
-size 28362
+oid sha256:fb036b1294457d0513f30be1452059e69d8d389cb22c06679158d4c7068ba58e
+size 29166
