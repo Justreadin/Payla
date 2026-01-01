fa03a7249b9d98d7e19816e6b9c04e5e1b362d93
Justreadin <dave.400g@gmail.com>
2026-01-01 15:36:50 +0100

    almost done

--- frontend/js/dashboard.js
+++ frontend/js/dashboard.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:477bf8495bbd90dfe76b963811d59b96a0e4549ea61e4f524fb397290236829b
-size 42802
+oid sha256:5ae979368e8937512e0de0388bdcaf23fc5021b929104a3dbfa63563b1784aad
+size 44396
